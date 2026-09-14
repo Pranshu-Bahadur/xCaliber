cce94b622121383506a64eb53333caf5905b2a6c
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-09-15 06:24:41 +0800

    x

--- xcaliber/inference/fused-moe/moe.py
+++ xcaliber/inference/fused-moe/moe.py
@@ -3,5 +3,12 @@ from __future__ import annotations
 from pycute import *
 
 
-def topk():
+def topk(router_logits : Tensor,
+    topk_idx : Tensor,
+    topk_weights : Tensor,
+    e_correction_bias : Tensor,
+    softmax : bool,
+    k : int,
+    n : int,
+    e : int):
     pass
