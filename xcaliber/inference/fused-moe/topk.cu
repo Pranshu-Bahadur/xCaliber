b2d685cda07ddc2fce654fd0d2b6b2a6b8f69fae
Pranshu Bahadur <pranshubahadur@gmail.com>
2026-09-18 04:14:06 +0800

    x

--- xcaliber/inference/fused-moe/topk.cu
+++ xcaliber/inference/fused-moe/topk.cu
@@ -123,7 +123,6 @@ __global__ void topk_kernel(
             local_topk[i].x = __float_as_uint(fmaf(__uint_as_float(local_topk[i].x), rW, 0.0f));
         }
     }
-    /*
     for (int i = 0; i < K; i++) {
         for (int j = 0; j < i; j++) {
             if (local_topk[j].x > local_topk[i].x) {
@@ -133,7 +132,6 @@ __global__ void topk_kernel(
             }
         }
     }
-    */
     local_minmax[1] = local_minmax[0];
     int pivot = K;
     for (int i = K-1; i >= 0; i--) {
